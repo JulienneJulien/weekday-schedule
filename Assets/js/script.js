0b4edf17f6a3188d7257ac63c7ccd1320d1fb3b7
Your Name <JulienneJulien15@gmail.com>
2022-12-06 13:54:03 -0500

    updated JAVA and HTML code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,57 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// ADDED DOCUMENT "READY." SO CODE WILL ONLY RUN ONCE THE PAGE (DOM) IS READY FOR JAVASCRIPT CODE TO EXECUTE
+$(document).ready(function () {
+// ADDED SAVEBTN CLICK LISTENER  
+$(".saveBtn").on ("click", saveHandler);
+
+function saveHandler () {
+  var inputText= $(this).siblings(".description").val ();
+  console.log("inputText= ", inputText);
+
+  var time= $(this).parent().attr("id").split("-")[1];
+  console.log("time= ", time);
+// SAVED USER INPUT TO LOCAL STORAGE  
+  localStorage.setItem(time,inputText);
+}
+
+function colorCoder () {
+// ADDED DAY.JS TO GET CURRENT HOUR IN 24-HOUR TIME 
+  var timeNow = dayjs().hour();
+// ADDED CODE TO APPLY THE CLASSES TO EACH TIME BLOCK SO BACKGROUND COLORS CAN BE INDICATED
+  $(".time-block").each (function() {
+    var blockTime= parseInt($(this).attr("id").split("-")[1]);
+    if (blockTime < timeNow){
+      $(this).addClass ("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+    } else if(blockTime === timeNow) {
+      $(this).removeClass ("past");
+       $(this).removeClass("future");
+      $(this).addClass ("present");
+    } else{
+      $(this).removeClass ("past");
+      $(this).addClass ("future");
+      $(this).removeClass ("present");
+    }
 });
+}
+
+colorCoder ();
+// ADDED CODE TO GET USER INPUT THAT WAS SAVED IN LOCALSTORAGE AND SET VALUES OF TEXT ELEMENTS
+  $('#hour-9 .description').val(localStorage.getItem('9'));
+  $('#hour-10 .description').val(localStorage.getItem('10'));
+  $('#hour-11 .description').val(localStorage.getItem('11'));
+  $('#hour-12 .description').val(localStorage.getItem('12'));
+  $('#hour-13 .description').val(localStorage.getItem('13'));
+  $('#hour-14 .description').val(localStorage.getItem('14'));
+  $('#hour-15.description').val(localStorage.getItem('15'));
+  $('#hour-16 .description').val(localStorage.getItem('16'));
+  $('#hour-17 .description').val(localStorage.getItem('17'));
+  $('#hour-18.description').val(localStorage.getItem('18'));
+  $('#hour-19 .description').val(localStorage.getItem('19'));
+  $('#hour-20 .description').val(localStorage.getItem('20'));
+
+// ADDED CODE TO DISPLAY CURRENT DATE IN THE HEADER  
+  var todayCurrrentDate = dayjs().format("	dddd, MMMM D, YYYY h:mm A");
+  $("#currentDay").text(todayCurrrentDate);
+})
+
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # weekday-schedule
+## Description
+<H4>I created a simple calendar application that allows a user to save events for each hour of the day.</H4>
+<li>This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.</li>
+<li>Applied acceptance criteria provided.</li>
+<li>Created a new repository on my GitHub account and cloned it to computer.</li>
+<li>Deployed project using git commands and pushed completed code to my GitHub repository.</li>
+
+## Link To Deployed Project/Application
+
+
+## Visuals
